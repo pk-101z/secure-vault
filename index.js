5e2d6ed7fb371aa745e4dec56e6c0d3dc829eb0e
pk-101z <potatoking101zzz@gmail.com>
2025-07-15 12:20:51 -0700

    finished assignment

---
+++ index.js
@@ -0,0 +1,17 @@
+
+let code1 = 7 + 3;
+
+let code2 = (100 - 20) / 2;
+
+let code3 = (15 % 8) * 6 - 3;
+
+let message = "The vault has been secured. The combination is:";
+
+let codeA = code1 + "-" + code2 + "-" + code3;
+
+
+let codeB = `${code1}-${code2}-${code3}`;
+
+console.log(message);
+console.log(codeA);
+console.log(codeB);
